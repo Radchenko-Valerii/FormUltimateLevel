24d184dfa70c5ece28a9ed9406b2eb3d69ee51bb
Valerii Radchenko <valerii.radchenko@freshcodeit.com>
2022-01-26 21:13:01 +0200

    Add routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,25 @@
 import React from 'react';
+import { BrowserRouter, Route, Router, Switch } from 'react-router-dom';
 import './App.css';
+import FirstStepPage from './pages/FirstStepPage/FirstStepPage';
+import FourthStepPage from './pages/FourthStepPage/FourthStepPage';
+import SecondStepPage from './pages/SecondStepPage/SecondStepPage';
+import ThirdStepPage from './pages/ThirdStepPage/ThirdStepPage';
+import FormState from './store/FormState';
 
 function App() {
-  return (
-    <div className="App">
-      form
-    </div>
+  console.log(FormState)
+  return ( 
+  <>
+  <BrowserRouter>
+  <Switch>
+  <Route exact path="/" component={FirstStepPage}/>
+  <Route exact path="/step2" component={SecondStepPage}/>
+  <Route exact path="/step3" component={ThirdStepPage}/>
+  <Route exact path="/step4" component={FourthStepPage}/>
+  </Switch>
+  </BrowserRouter>
+  </>
   );
 }
 
