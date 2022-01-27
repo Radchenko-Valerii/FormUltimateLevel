23e90c95b7d585887997d43d226a2029c71a19b9
Valerii Radchenko <valerii.radchenko@freshcodeit.com>
2022-01-27 19:20:22 +0200

    Add new types

---
+++ src/types/types.ts
@@ -0,0 +1,26 @@
+import { ReactNode } from "react"
+
+export interface IDataContext {
+
+}
+
+export interface IInput {
+  props?: any,
+  ref?: any,
+  id: string,
+  type: string,
+  label: string,
+  name: string,
+  onChange?: (e: any) => any
+}
+
+export interface IDefaultComponent {
+  props?: any,
+  children?: ReactNode
+}
+
+export interface IFormComponent {
+  props?: any,
+  children?: ReactNode,
+  onSubmit: (e: React.FormEvent) => any
+}
